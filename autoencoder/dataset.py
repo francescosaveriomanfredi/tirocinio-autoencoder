bf6f3d77bab5c73e0a50127a3b98ff381dd78f16
Francesco Saverio Manfredi <fmsaverio@yahoo.it>
2022-12-29 22:56:37 +0100

    Improve dataset library with DataModule

--- autoencoder/dataset.py
+++ autoencoder/dataset.py
@@ -1,10 +1,40 @@
 import anndata as an
 import numpy as np
-from typing import Optional, Callable, Union
+from typing import Optional, Callable, Union, Optional, Sequence
 from torch.utils import data
 from scipy.sparse import issparse
 from scipy.stats import binom
 from sklearn.preprocessing import StandardScaler, MaxAbsScaler
+import pytorch_lightning as pl
+from torch.utils.data import random_split, DataLoader
+
+def filter_gene(
+    adata:an.AnnData,
+    gene_filter:Optional[str]="highly_variable",
+    n_gene:Optional[int]=None,
+):
+    """
+    Filter the AnnData based on its var field.
+
+    parameters
+    -----------
+    adata 
+        AnnData
+    gene_filter
+        Var field name used to filter the data.
+    n_gene
+        If None gene_filter var must contains boolean value
+        else gene_filter var must contains integer value.
+        high value is better. 
+    """
+    if gene_filter is None:
+        gene_index = adata.var_names
+    elif n_gene is None:
+        gene_index = adata.var[gene_filter].index
+    else:
+        gene_index = adata.var[gene_filter].nlargest(n_gene).index
+    
+    return adata[:, gene_index].copy()
 
 class Scaler:
     def __init__(self, n, scaler):
@@ -16,10 +46,9 @@ class BinomDropout:
     def __init__(self, p):
         self.p = p
     def __call__(self, n):
-        n
         return n - binom.rvs(n, self.p)
 
-class MyDataset(data.Dataset):
+class CountDataset(data.Dataset):
     """
     A Dataset of genes count
     
@@ -34,19 +63,22 @@ class MyDataset(data.Dataset):
     """
     def __init__(
         self,
-        data: any,
-        scale_type: Union[str, Callable] = "StandardScaler",
-        p_gene_dropout: float = 0.
+        adata: an.AnnData,
+        gene_filter: Optional[str]="highly_variable",
+        n_gene: Optional[int]=None,
+        scale_type: Union[str, Callable, None]=None,
+        p_gene_dropout: float=0.
     ):
         super().__init__()
-        self.data = data
-        self.dim = data.shape[0]
+        adata = filter_gene(adata, gene_filter, n_gene)
+        self.data = adata.X
+        self.dim = self.data.shape[0]
         if scale_type is None:
             self.scaler = lambda x:x
         elif scale_type == "StandardScaler":
-            self.scaler = Scaler(data, StandardScaler(with_mean=False))
+            self.scaler = Scaler(self.data, StandardScaler(with_mean=False))
         elif scale_type == "MaxAbsScaler":
-            self.scaler = Scaler(data, MaxAbsScaler())
+            self.scaler = Scaler(self.data, MaxAbsScaler())
         else:
              self.scaler = scale_type
         self.gene_dropout = BinomDropout(p_gene_dropout)
@@ -59,3 +91,76 @@ class MyDataset(data.Dataset):
         out_data = self.gene_dropout(out_data.astype(np.int32)).astype(np.float32)
         in_data = self.scaler(out_data).astype(np.float32)
         return in_data.squeeze(0), out_data.squeeze(0)
+
+
+class CountDataModule(pl.LightningDataModule):
+    def __init__(
+        self,
+        adata:Union[an.AnnData, str],
+        batch_size:int=128,
+        n_gene:Optional[int]=None,
+        gene_filter:Optional[str]="highly_variable",
+        scale_type:Union[Callable, str, None]="StandardScaler",
+        p_gene_dropout:float=0.,
+        split_lengths:Sequence[int|float]=[0.7, 0.2, 0.1],
+        num_workers:int=0,
+    ):
+        super().__init__()
+        self.adata=adata
+        self.batch_size = batch_size
+        self.n_gene = n_gene
+        self.gene_filter = gene_filter
+        self.scale_type = scale_type
+        self.p_gene_dropout = p_gene_dropout
+        self.split_lenghts = split_lengths
+        self.num_workers = num_workers
+    
+    def setup(self, stage=None):
+        # If we give a path and not an AnnData
+        if isinstance(self.adata, str):
+            self.adata = an.read_h5ad("../counts.h5ad.gzip")
+        
+        self.count_predict = CountDataset(
+            adata=self.adata,
+            gene_filter=self.gene_filter,
+            n_gene=self.n_gene,
+            scale_type=self.scale_type,
+            p_gene_dropout=self.p_gene_dropout
+        )
+
+        self.count_train, self.count_val, self.count_test = random_split(
+            self.count_predict, 
+            self.split_lenghts
+        )
+        
+    def train_dataloader(self):
+        return DataLoader(
+            self.count_train, 
+            batch_size=self.batch_size,
+            num_workers=self.num_workers,
+            pin_memory=True
+        )
+
+    def val_dataloader(self):
+        return DataLoader(
+            self.count_val, 
+            batch_size=self.batch_size,
+            num_workers=self.num_workers,
+            pin_memory=True
+        )
+
+    def test_dataloader(self):
+        return DataLoader(
+            self.count_test, 
+            batch_size=self.batch_size,
+            num_workers=self.num_workers,
+            pin_memory=True
+        )
+
+    def predict_dataloader(self):
+        return DataLoader(
+            self.count_train, 
+            batch_size=self.batch_size,
+            num_workers=self.num_workers,
+            pin_memory=True
+        )
