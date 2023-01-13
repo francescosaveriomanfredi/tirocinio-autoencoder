3fd602ecab755eddad702fc81d0846895dd3837a
francescosaveriomanfredi <fmsaverio#@yahoo.it>
2023-01-13 04:44:49 +0100

    Reordering of datasets.

--- autoencoder/dataset.py
+++ autoencoder/dataset.py
@@ -1,17 +1,51 @@
+import urllib.request 
+import os
 import anndata as an
+import pandas as pd
 import numpy as np
 from typing import Optional, Callable, Union, Optional, Sequence
 from torch.utils import data
 from scipy.sparse import issparse
 from scipy.stats import binom
+import scanpy as sc
 from sklearn.preprocessing import StandardScaler, MaxAbsScaler
 import pytorch_lightning as pl
 from torch.utils.data import random_split, DataLoader
+import urllib.request
+
+def download(filename, url, destination_dir, expected_bytes = None, force = False):
+    if url is None:
+      if filename == "count1":
+            url = "https://drive.google.com/uc?export=download&id=1Mda9KAWnX4JBTm8EHTIfQ6DqxkT3lXsL"
+    
+    filepath = os.path.join(destination_dir, filename)
+
+    if force or not os.path.exists(filepath):
+        if not os.path.exists(destination_dir):
+            os.makedirs(destination_dir)
+
+        print('Attempting to download: ' + filename)
+        filepath, _ = urllib.request.urlretrieve(url, filepath,)
+        print('Download complete!')
+
+    statinfo = os.stat(filepath)
+
+    if expected_bytes != None:
+        if statinfo.st_size == expected_bytes:
+            print('Found and verified: ' + filename)
+        else:
+            raise Exception('Failed to verify: ' + filename + '. Can you get to it with a browser?')
+    else:
+        print('Found: ' + filename)
+        print('The size of the file: ' + str(statinfo.st_size))
+
+    return filepath
 
 def filter_gene(
     adata:an.AnnData,
     gene_filter:Optional[str]="highly_variable",
     n_gene:Optional[int]=None,
+    gene_filter_ascending=True,
 ):
     """
     Filter the AnnData based on its var field.
@@ -25,14 +59,20 @@ def filter_gene(
     n_gene
         If None gene_filter var must contains boolean value
         else gene_filter var must contains integer value.
-        high value is better. 
+        high value is better.
+    gene_filter_ascending
+         If select largest or smallest based on value
     """
+    if gene_filter_ascending:
+        n_row = pd.Series.nlargest
+    else:
+        n_row = pd.Series.nsmallest
     if gene_filter is None:
         gene_index = adata.var_names
     elif n_gene is None:
         gene_index = adata.var_names[adata.var[gene_filter]]
     else:
-        gene_index = adata.var[gene_filter].nlargest(n_gene).index
+        gene_index = n_row(adata.var[gene_filter], n_gene).index
     
     return adata[:, gene_index].copy()
 
@@ -60,26 +100,44 @@ class CountDataset(data.Dataset):
     
     Parameters
     ----------
-    data
-        raw counts in a sparse or not sparse format
-    scale
-        A callable used to scale data 
+    data_path
+        path to the count gene expression adata
+    scale_type
+        A callable used to scale data or
+        StandardScaler, BinarizeScaller
     p_gene_dropout
         probability to drop one gene espression for each gene expression
+    n_gene
+        the number of gene select using gene_filter var field
+    gene_filter
+         a adata var field using to select the gene
+    gene_filter_ascending
+         the order to select the genes based on gene_filter
+    
     """
     def __init__(
         self,
-        adata: an.AnnData,
+        adata_path: str,
         scale_type: Union[str, Callable, None]=None,
-        p_gene_dropout: float=0.
+        p_gene_dropout: float=0. ,
+        n_gene:Optional[int]=None,
+        gene_filter:Optional[str]=None,
+        gene_filter_ascending:bool=True,
+        #url:Optional[str]=None
     ):
         super().__init__()
-        self.data = adata.X
-        self.dim = self.data.shape[0]
+        self.adata = an.read_h5ad(adata_path)
+        self.adata = filter_gene(
+            self.adata, 
+            gene_filter, 
+            n_gene,
+            gene_filter_ascending
+        )
+        self.dim = self.adata.n_obs
         if scale_type is None:
             self.scaler = lambda x:x
         elif scale_type == "StandardScaler":
-            self.scaler = Scaler(self.data, StandardScaler(with_mean=False))
+            self.scaler = Scaler(self.adata.X, StandardScaler(with_mean=False))
         elif scale_type == "BinarizeScaler":
             self.scaler = Binarize()
         else:
@@ -90,7 +148,7 @@ class CountDataset(data.Dataset):
         return self.dim
     
     def __getitem__(self, idx):
-        out_data = self.data[idx].toarray() if issparse(self.data) else self.data[idx]
+        out_data = self.adata.X[idx].toarray() if issparse(self.adata.X) else self.data.X[idx]
         out_data = self.gene_dropout(out_data.astype(np.int32)).astype(np.float32)
         in_data = self.scaler(out_data).astype(np.float32)
         return {
@@ -100,42 +158,69 @@ class CountDataset(data.Dataset):
 
 
 class CountDataModule(pl.LightningDataModule):
+    """
+    Parameters
+    ----------
+    data_path
+        path to the count gene expression adata
+    batch_size
+        the batch size of data to feed the module
+    n_gene
+        the number of gene select using gene_filter var field
+    gene_filter
+         a adata var field using to select the gene
+    gene_filter_ascending
+         the order to select the genes based on gene_filter
+    scale_type
+        A callable used to scale data or
+        StandardScaler, BinarizeScaller
+    p_gene_dropout
+        probability to drop one gene espression for each gene expression
+    train_val_test_lengths
+        The train validation test partition to split data
+    num_workers
+        The number of thread to use.
+    """
     def __init__(
         self,
-        adata:Union[an.AnnData, str],
+        adata_path:str,
         batch_size:int=128,
         n_gene:Optional[int]=None,
         gene_filter:Optional[str]=None,
+        gene_filter_ascending:bool=True,
         scale_type:Union[Callable,str,None]="StandardScaler",
         p_gene_dropout:float=0.,
         train_val_test_lengths:Sequence[float]=[0.7, 0.2, 0.1],
         num_workers:int=0,
+        #source:Optional[str]=None
     ):
         super().__init__()
-        self.adata=adata
+        self.save_hyperparameters()
+        self.adata_path=adata_path
         self.batch_size = batch_size
         self.n_gene = n_gene
         self.gene_filter = gene_filter
+        self.gene_filter_ascending = gene_filter_ascending
         self.scale_type = scale_type
         self.p_gene_dropout = p_gene_dropout
         self.train_val_test_lengths = train_val_test_lengths
         self.num_workers = num_workers
+        #self.source=source
+    
+    def prepare_data(self):
+        # download
+        pass
     
     def setup(self, stage=None):
-        # If we give a path and not an AnnData
-        if isinstance(self.adata, str):
-            self.adata = an.read_h5ad(f"{self.adata}")
-        
-        self.adata = filter_gene(
-            self.adata, 
-            self.gene_filter, 
-            self.n_gene
-        )
         self.count_predict = CountDataset(
-            adata=self.adata,
+            adata_path=self.adata_path,
             scale_type=self.scale_type,
-            p_gene_dropout=self.p_gene_dropout
+            p_gene_dropout=self.p_gene_dropout,
+            gene_filter=self.gene_filter, 
+            n_gene=self.n_gene,
+            gene_filter_ascending=self.gene_filter_ascending
         )
+        self.adata = self.count_predict.adata
 
         self.count_train, self.count_val, self.count_test = random_split(
             self.count_predict, 
@@ -187,6 +272,10 @@ def highly_variable_genes(adata, n_top_genes):
     return adata_
 
 def unbatch(batched_param):
+    """
+    utility function used to 
+    unbatch the result of a prediction.
+    """
     n_param = len(batched_param[0])
     unbatched_param = []
     for param in range(n_param):
@@ -196,6 +285,9 @@ def unbatch(batched_param):
     return unbatched_param
 
 def annotate_predictions(adata, layers, obsm, obs,):
+    """
+    Add information to the adata passed
+    """
     for layers_label in layers:
         adata.layers[layers_label] = layers[layers_label]
     for obsm_label in obsm:
--- autoencoder/plot.py
+++ autoencoder/plot.py
@@ -1,52 +1,134 @@
-import seaborn as sns
+import os
+import yaml
 from sklearn.decomposition import PCA
 import pandas as pd
+import numpy as np
 import plotly.express as px
+import plotly.graph_objects as go
 
-def joinplot_pca(df:pd.DataFrame, cluster=None, palette=None, size=10, alpha=0.3):
-    pca = PCA(n_components=2)
-    pca_df= pd.DataFrame(pca.fit_transform(df), columns=["pca_1", "pca_2"])
-    sns.jointplot(data=pca_df, x="pca_1", y="pca_2", hue=cluster, alpha=alpha, palette=palette, height=size)
-    return pca_df
+def plot_CSV_collection(
+    base_dir="./lightning_logs",
+    metrics=None,
+    train=True,
+    val=True
+):   
+    """
+    function to fancy plot a list of metric 
+    reported by multiple pytorch lighting csv 
+    logger with the same name, and dir
 
-def plot_CSVLogger(data, metrics,):
-    try:
-        history = pd.read_csv(data, index_col="epoch")
-    except:
-        history = data
-    history = history.groupby(level=0).mean()
-    fig = px.line(history)
-    fig = fig.update_layout(
+    Parameters
+    ----------
+    base_dir
+        The name of the directory with 
+        subdirectory called version_n  
+        each subdirectory contains a file called metrics.csv
+    metrics
+        The subsample of metric to consider,
+        example {nb_loss, kl_loss, crossentropy, recall, ...}
+        if None take all metric
+    val
+        Choice if include val metrics in the plot
+    train
+        Choice if include train metrics in the plot
+    """
+    dirs = os.listdir(base_dir)
+    palettes = [px.colors.qualitative.Set1, px.colors.qualitative.Pastel1]
+    modes = list(l for l, c in  zip(["val", "train"], [val, train]) if c) 
+    button_label = []
+    fig = go.Figure()
+
+    for i, dir in enumerate(dirs):
+      # leggo il file csv
+      df = pd.read_csv(
+          base_dir+dir+"/metrics.csv",
+          index_col="epoch"
+      ).groupby(level=0).mean()
+      for mode, palette in zip(modes, palettes):
+        # filtro per modo 
+        df_mode = df.filter(like=mode, axis=1)
+        df_mode.columns = df_mode.columns.str.replace(mode+"_","")
+        # se le metriche non sono specificate le prendo tutte
+        if metrics is None:
+          metrics=df_mode.columns
+        for metric in (metrics):
+          # seleziono la metrica
+          sr_metric = df_mode.loc[:,df_mode.columns.str.contains(metric)].squeeze()
+          # salto se la metrica non e presente
+          if sr_metric.shape[0] == 0: continue
+          fig.add_trace(go.Scatter(
+                            x=sr_metric.index,
+                            y=sr_metric.values,
+                            visible=True,
+                            name=dir+"/"+mode,
+                            line=dict(color=palette[i])
+                           )
+                )
+          button_label.append(metric)
+    
+    # create button
+    buttons = []
+    button_label = np.array(button_label)
+    for metric in set(metrics):
+      traces = button_label == metric
+      buttons.append(dict(method='update',
+                          label=metric,
+                          visible=True,
+                          args=[{'visible': traces}],
+                          )
+                  )
+    # create the layout 
+    layout = go.Layout(
         updatemenus=[
             dict(
-                type="buttons",
-                direction="right",
-                x=0.7,
+                type='buttons',
+                direction='right',
+                x=0.8,
                 y=1.2,
                 showactive=True,
-                buttons=list(
-                    [
-                    
-                        dict(
-                            label=metric,
-                            method="update",
-                            args=[
-                                {"visible": list(history.columns.str.endswith(metric))},
-                                {"yaxis.title.text": metric},
-                            ],
-                        )for metric in metrics
-                    ]
-                ),
+                buttons=buttons
             )
-        ]
+        ],
+        title=dict(text='metric plot'),
+        showlegend=True
     )
-    return fig
+    return fig.update_layout(layout)
+
+
+def df_yaml_collection(
+    base_dir="./lightning_logs",
+    hparams_label=None,
+):
+    """
+    function to return a dataframe  with the parameter 
+    of each model.
+    """
+    dirs=os.listdir(base_dir)
+    filename="hparams.yaml"
+    df = pd.DataFrame()
+    hparams={}
+    cells = []
+    fig = go.Figure()
+    for sub_dir in dirs:
+      with open("/".join([base_dir,sub_dir,filename]), 'r') as file:
+          hparams = yaml.safe_load(file)
+
+      df=df.append(
+          {label: hparams[label] for label in hparams}, 
+          ignore_index=True
+          )
+    if hparams_label is not None:
+        df = df[hparams_label]
+
+    df.index = pd.Index(dirs, name="version")
+    return df
 
 def plot_scatter(
     X_pca, 
     variance_ratio=None, 
     color=None, 
     color_discrete_sequence=None,
+    symbol=None
 ):
     if variance_ratio is None:
         variance_ratio = ["",""]
@@ -68,16 +150,28 @@ def plot_scatter(
         opacity=0.3,
         marginal_x="histogram",
         marginal_y="histogram",
-        #range_color=[0,1]
+        #color_continuous_midpoint=color.quantile(0.75)
+        range_color=[0, color.quantile(0.8)],
+        symbol=symbol
     )
-
+    
     fig.update_layout(title = "PCA",
                       xaxis_title = f"PCA1"+variance_ratio[0],
                       yaxis_title = f"PCA2"+variance_ratio[1],
                       width = 2000,
                       height = 700,
+                      #legend_y = 1.05,
+                      #legend_x = 1.035,
+                      coloraxis_colorbar_x = -0.10
                       )
 
     config = dict({'scrollZoom': True})
     # add dropdown menus to the figure
     return fig.show(config=config)
+
+
+
+
+
+
+
